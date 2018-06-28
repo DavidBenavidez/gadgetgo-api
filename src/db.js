ae9595b6c4507917cd09ae4a3daac5281ba640a1
DavidBenavidez <ddbenavidez2@up.edu.ph>
2018-06-29 06:34:11 +0800

    Changed Database name

--- src/db.js
+++ src/db.js
@@ -2,8 +2,10 @@ import mongoose from 'mongoose';
 
 mongoose.Promise = global.Promise;
 
-const db = mongoose.createConnection('mongodb://localhost/gadgetgo');
-
+// const db = mongoose.createConnection('mongodb://localhost/gadgetgo');
+const db = mongoose.createConnection(
+  'mongodb://RadicalRalph:sectum1@ds018168.mlab.com:18168/gadgetgo',
+);
 db.on('error', err => console.log(err));
 
 db.once('open', () => console.log('Successfully connected to MongoDB'));
